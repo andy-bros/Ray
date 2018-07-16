8dd6417b86705744ba852c4c307ccdd13e2ba662
Jacob Anderson <odistiinct@gmail.com>
2018-07-16 10:40:08 -0500

    initial set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <h1>what up github</h1>
       </div>
     );
