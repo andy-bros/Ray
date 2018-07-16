ae128c61466a127ff5f7b9ba50e933ab5af9c73b
Jacob Anderson <odistiinct@gmail.com>
2018-07-16 11:20:17 -0500

    stripe backend

--- server/index.js
+++ server/index.js
@@ -5,8 +5,12 @@ const express = require("express"),
   { json } = require("body-parser"),
   PORT = process.env.PORT || 3001;
 
+const stripe = require("./controllers/stripe");
+
 app.use(json());
 app.use(cors());
+
+app.post("/charge", stripe);
 
 app.listen(PORT, () => {
   console.log(`Magic Happens on port ${PORT}`);
