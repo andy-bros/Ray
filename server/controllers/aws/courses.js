203d2d18262786234764c55e99e1cd8c90f4b706
Jacob Anderson <odistiinct@gmail.com>
2018-07-19 15:37:14 -0500

    test

--- server/controllers/aws/courses.js
+++ server/controllers/aws/courses.js
@@ -3,3 +3,14 @@ const s3 = new AWS.S3({
   secretAccessKey: process.env.AWSSecretKey,
   accessKeyId: process.env.AWSAccessKeyId
 });
+
+const getCourses = () => {
+  s3.listObjects(
+    { Bucket: "raymp3s", Prefix: "Courses/", Delimiter: "/" },
+    function(err, res) {
+      if (err) console.log(err);
+      if (res) console.log(res);
+    }
+  );
+};
+getCourses();
