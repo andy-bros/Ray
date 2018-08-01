575ad4ae529d472378921bbf82f9e36211d85c58
Zac Cericola <zcericola@gmail.com>
2018-08-01 17:11:14 -0500

    worked on splash page accents

--- src/components/Navbar/Footer.js
+++ src/components/Navbar/Footer.js
@@ -1,24 +1,24 @@
-import React from "react";
+import React from 'react';
 
 export default function Footer() {
   return (
     <footer className="footer-container">
       <section className="information">
-        <h4 className="bottom-border">FIND US</h4>
+        <h4 className="footer-title">FIND US</h4>
 
         <span>
-          <h4>Address</h4>
+          <h4 className="footer-headings">Address</h4>
           <p>
             Meets at Oak Hill Assembly of God 5200 Franklin Pike Nashville, TN
             37220
           </p>
         </span>
         <span>
-          <h4>Service Hours</h4>
+          <h4 className="footer-headings">Service Hours</h4>
           <p>Saturday nights from 5:00-6:30 PM</p>
         </span>
         <span>
-          <h4>Contact</h4>
+          <h4 className="footer-headings">Contact</h4>
           <p>info@ccnashville.com</p>
         </span>
       </section>
