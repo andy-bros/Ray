31caaa7b42a33c993d045e47b0070b2f42a80214
josephiznot <josephiznot@gmail.com>
2018-08-02 09:43:33 -0500

    functionality for cart and checkout

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -2,7 +2,12 @@ import React, { Component } from "react";
 import { Credentials } from "./../Donate/DonateForm";
 import { SubmitButton } from "./../Donate/Customs";
 import { connect } from "react-redux";
-import { getCart } from "./../../redux/cartReducer";
+import {
+  getCart,
+  updateCart,
+  deleteFromCart,
+  emptyCart
+} from "./../../redux/cartReducer";
 class Checkout extends Component {
   constructor() {
     super();
@@ -29,38 +34,53 @@ class Checkout extends Component {
   }
   handleChange = ({ event, value }) => {
     [event.target.name][0] !== "checked" && event.preventDefault();
+    console.log(value);
     this.setState({
       [event.target.name]: value
     });
   };
-  submitForm = () => {
+  submitForm = event => {
     console.log("submitting...", this.state);
+    this.props.emptyCart();
+    this.props.history.push("/");
   };
   componentDidMount() {
     this.props.getCart();
   }
+  handleQuantity = (id, val) => {
+    let { cart } = this.props;
+    const i = cart.findIndex(e => e.product_id == id);
+    cart[i].quantity = val;
+  };
+  dispatchUpdate = () => {
+    this.props.updateCart(this.props.cart);
+  };
   render() {
-    console.log(this.props.cart);
     const cart = this.props.cart.map((e, i) => {
-      console.log(e);
       return (
         <div key={i}>
           <h5>{e.product_name}</h5>
-          {/* <input type="number" value={e.quantity || 1}> */}
+          <input
+            type="number"
+            min="1"
+            max="5"
+            defaultValue={e.quantity || 1}
+            name={e.product_name}
+            onChange={event =>
+              this.handleQuantity(e.product_id, event.target.value)
+            }
+          />
           <h6>quantity: {e.quantity || 1}</h6>
-          {/* </input> */}
+          <button onClick={() => this.props.deleteFromCart(e.product_id)}>
+            REMOVE FROM CART
+          </button>
         </div>
       );
     });
     return (
-      <form
-        className="donation-page"
-        onSubmit={e => {
-          e.preventDefault();
-          this.submitForm();
-        }}
-      >
+      <div className="donation-page">
         {cart}
+        <button onClick={this.dispatchUpdate}>UPDATE CART</button>
         <Credentials
           values={this.state}
           inputFields={this.state.inputFields}
@@ -68,12 +88,12 @@ class Checkout extends Component {
           selectBox={true}
           purpose="Shipping"
         />
-        <SubmitButton text="Submit Order" />
-      </form>
+        <SubmitButton text="Submit Order" submitForm={this.submitForm} />
+      </div>
     );
   }
 }
 export default connect(
   state => state.cartReducer,
-  { getCart }
+  { getCart, updateCart, deleteFromCart, emptyCart }
 )(Checkout);
