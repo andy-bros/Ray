6e4d016932cecac68f9b86f9a2fe4d46e8579f2b
josephiznot <josephiznot@gmail.com>
2018-08-05 08:58:34 -0500

    starting breadcrumb

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Utils from "./utils";
-
+import BreadCrumb from "./../BreadCrumb/BreadCrumb";
 export default function Courses() {
   return (
     <div className="section-containers">
+      <BreadCrumb />
       <h2 className="section-titles">MESSAGES</h2>
       <Utils type="Messages" />
     </div>
